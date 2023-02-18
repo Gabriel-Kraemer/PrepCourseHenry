83133faff9a343ebca11389eed7d18e39429a0fe
Gabriel_Kraemer <gabrielkraemer0@gmail.com>
2023-02-18 01:00:56 -0300

    primer día de ejercicios

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -5,32 +5,36 @@
 
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
-   // Tu código:
+   return (num * num);
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
-   // Tu código:
+   return (num * num * num);
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
-   // Tu código:
+   let res = num;
+   for (let i = exponent; i > 1; i--){
+      res *= num;
+   }
+   return res;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
-   // Tu código:
+   return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
-   // Tu código:
+   return Math.ceil(num);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
-   // Tu código:
+   return Math.random();
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
